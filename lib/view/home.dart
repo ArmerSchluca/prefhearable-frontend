933717bf33cf2c605578a9c69c2b3e90ba85316f
Armer Schluca <reichow.2000@gmail.com>
2026-06-18 01:38:29 +0200

    Design for SectionCards on HomeScreen

--- lib/view/home.dart
+++ lib/view/home.dart
@@ -9,17 +9,93 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Survey App", style: TextStyle(color: Colors.amberAccent)),
-        backgroundColor: Colors.teal,
+        title: Text("Prefhearable | Hearing Preference Survey App", style: TextStyle(color: Colors.white)),
+        backgroundColor: Colors.black,
       ),
-      body: Column(
-        children: [
-          Text("Personal Data"),
-          Text("Context Data"),
-          Text("Audio Tests"),
-          Text("Questionnaires"),
-        ],
+
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: .start,
+          children: [
+            SectionCard(
+              title: "Personal Data",
+              icon: Icon(
+                Icons.person, 
+                color: Colors.amber
+                ),
+              status: SectionStatus.open,
+            ),
+            SizedBox(height: 12),
+            SectionCard(
+              title: "Contextual Data",
+              icon: Icon(
+                Icons.public, 
+                color: Colors.green
+                ),
+              status: SectionStatus.open,
+            ),
+            SizedBox(height: 12),
+            SectionCard(
+              title: "Audio Tests",
+              icon: Icon(
+                Icons.headphones, 
+                color: Colors.pink
+                ),
+              status: SectionStatus.open,
+            ),
+            SizedBox(height: 12),
+            SectionCard(
+              title: "Questionnaires",
+              icon: Icon(
+                Icons.assignment, 
+                color: Colors.blue
+                ),
+              status: SectionStatus.open,
+            ),
+          ],
+        ),
       ),
     );
   }
 }
+
+class SectionCard extends StatelessWidget {
+  final String title;
+  final Icon icon;
+  final SectionStatus status;
+
+  const SectionCard({
+    super.key,
+    required this.title,
+    required this.icon,
+    required this.status,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Card(
+        clipBehavior: .hardEdge,
+        child: InkWell(
+          splashColor: Colors.blue.withAlpha(30),
+          onTap: () {
+            debugPrint('Card tapped.');
+          },
+          child: Column(
+            mainAxisSize: .min,
+            children: <Widget>[
+              ListTile(
+                leading: icon,
+                title: Text(title),
+                subtitle: Text(status.toString()),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+enum SectionStatus { open, partial, completed }
