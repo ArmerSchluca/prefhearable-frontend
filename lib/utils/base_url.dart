5324afafaaaa3b7fe552ef9ded6e24defda751a1
Armer Schluca <reichow.2000@gmail.com>
2026-07-20 23:40:36 +0200

    Stelle baseUrl zurück auf Dev

--- lib/utils/base_url.dart
+++ lib/utils/base_url.dart
@@ -1,3 +1,10 @@
+import 'dart:io';
+
+// Gibt je nach Plattform die korrekte baseUrl aus
 String get baseUrl {
-  return "https://api-prefhearable.rchw.de";
+  if (Platform.isAndroid) {
+    return "http://10.0.2.2:3000";
+  }
+  // Windows / macOS / Linux / iOS / Browser
+  return "http://localhost:3000";
 }
